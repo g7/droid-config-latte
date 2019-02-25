2519d68c16b40d5a65e2bbce064d85dcc5142823
Eugenio Paolantonio (g7) <me@medesimo.eu>
2019-02-25 02:40:44 +0100

    [droid-config-latte] Added systemd service that adjusts the sampling frequency of the IIO accelerometer
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

---
+++ sparse/usr/bin/droid/iio_increase_sampling_frequency.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+error() {
+	echo "E: ${@}"
+	exit 1
+}
+
+# systemd is dumb and doesn't support Restart=on-failure on oneshot scripts...
+for try in $(seq 5); do
+
+	sampling_frequency_file=$(find /sys/bus/iio/devices -type f -name in_accel_sampling_frequency -follow 2> /dev/null | head -n1)
+
+	if [ "${sampling_frequency_file}" ]; then
+	        echo 100 > ${sampling_frequency_file}
+		break
+	fi
+
+	sleep 5
+done
+
+[ -z "${sampling_frequency_file}" ] && error "Unable to find in_accel_sampling_frequency"
+
+exit 0
+
